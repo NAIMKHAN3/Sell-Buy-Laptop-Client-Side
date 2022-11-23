b7f01d20c930ae1591e22e1fa2706128978315fd
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-11-24 03:03:44 +0600

    navbar added and responsive

--- src/Page/Share/Navbar/Navbar.js
+++ src/Page/Share/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import laptop from '../Navbar/image/laptop.jpg'
 
 const Navbar = () => {
 
@@ -16,40 +17,48 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100 px-10">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <div className='mt-7'>
+            <div className='flex justify-center items-center'>
+                <img className='w-16 mr-3' src={laptop} alt="" />
+                <Link to='/'> <p className=" btn-ghost text-4xl font-bold text-orange-400 text-center">Sell-Buy-Laptop</p> </Link>
+            </div>
+            <div className="navbar bg-success px-8">
+
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {
+                                item
+                            }
+                        </ul>
+                    </div>
+
+                    {/* <Link to='/' className="btn btn-ghost normal-case text-xl text-orange-400">SELL-Buy-Laptop</Link> */}
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
                         {
                             item
                         }
                     </ul>
                 </div>
+                <div className="avatar navbar-end">
+                    <div className="dropdown">
+                        <label htmlFor="deshboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        {/* <label  className="btn btn-primary drawer-button">Open drawer</label> */}
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 
-                <Link to='/' className="btn btn-ghost normal-case text-xl">Doctor Portel</Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    {
-                        item
-                    }
-                </ul>
-            </div>
-            <div className="avatar navbar-end">
-                <div className="dropdown">
-                    <label htmlFor="deshboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    {/* <label  className="btn btn-primary drawer-button">Open drawer</label> */}
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
+        </div>
+
 
     );
 };
