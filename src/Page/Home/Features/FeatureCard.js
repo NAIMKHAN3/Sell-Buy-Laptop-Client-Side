b7c5d4a8272c02564cd97d0362eea9296666404a
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-02-23 00:49:49 +0600

    FearureCard Added

--- src/Page/Home/Features/FeatureCard.js
+++ src/Page/Home/Features/FeatureCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const FeatureCard = ({ feature }) => {
+    console.log(feature)
     return (
-        <div>
-            <h1>This Is Nsi</h1>
+        <div className='border-2 border-indigo-700 m-2 p-2 bg-indigo-200'>
+            <h1 className='text-3xl font-bold text-indigo-700 text-center'>{feature.name}</h1>
+            <p className='p-3'>{feature.details}</p>
         </div>
     );
 };
--- src/Page/Home/Features/Features.js
+++ src/Page/Home/Features/Features.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import FeatureCard from './FeatureCard';
 
 const Features = () => {
 
-    const brands = [
+    const features = [
         {
             name: "Seller",
             details: "As a seller on a laptop website, you have a unique opportunity to tap into a growing market of tech-savvy customers looking for high-quality laptops and accessories. Here are some of the top opportunities for sellers on a laptop website. High demand: Laptops are in high demand, as more people are working remotely and relying on technology for work and entertainment. By selling laptops and accessories on a website, you can tap into this growing market and meet the needs of customers who are looking for high-quality, reliable devices."
@@ -11,11 +12,20 @@ const Features = () => {
             name: "Buyer",
             details: "As a buyer on a seller laptop website, you have a unique opportunity to find the perfect laptop and related accessories that meet your needs and budget. Here are some of the top opportunities for buyers on a seller laptop website. Wide range of options: A laptop seller website can offer a wide range of laptops and related accessories from various brands, price points, and configurations. This gives you the opportunity to compare and choose the best option that meets your specific needs."
         },
+        {
+            name: "Feature",
+            details: "This website has a responsive nav bar to navigate the website easily and a modern-looking footer. The website has some essential components, like a banner and an advertisement section with several product categories. I implemented authorization and authentication systems to secure the user and website data on this website. The vital part of this website is the user and admin route; with these routes, anyone can use this website efficiently."
+        },
     ]
 
     return (
-        <div>
-            <h1 className='text-4xl font-bold text-indigo-600 text-center mt-10'>Features</h1>
+        <div className='mx-3'>
+            <h1 className='text-4xl font-bold text-indigo-600 text-center my-10'>Features</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3'>
+                {
+                    features.map(feature => <FeatureCard key={feature.name} feature={feature}></FeatureCard>)
+                }
+            </div>
         </div>
     );
 };
