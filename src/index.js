0de3453cdd12bc78242437e6395e9dfd0a1e9edf
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-30 00:16:59 +0600

    redux and redux-toolkit install and initial setup successfully

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserContext from './Page/Share/UserContex/UserContext';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <UserContext>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </UserContext>
   </React.StrictMode>
 );
